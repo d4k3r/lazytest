9e08a7de424522c08bf7e35b9145d5eee52e8674
d4k3r <53003498+d4k3r@users.noreply.github.com>
2025-12-11 09:10:17 +0000

    test: AI Generated tests (6 failed, 5 passed in 0.06s)

--- tests/generated/test_code_for_tests.py
+++ tests/generated/test_code_for_tests.py
@@ -14,59 +14,58 @@ def test_case_1():
 
 
 def test_case_2():
-    int_0 = 4083
-    none_type_0 = None
-    module_0.add(int_0, none_type_0)
+    int_0 = 1167
+    var_0 = module_0.add(int_0, int_0)
+    assert var_0 == 2334
 
 
 def test_case_3():
-    str_0 = "C6\x0bK\tf_o=,bsc"
-    var_0 = module_0.reverse_string(str_0)
-    assert var_0 == "csb,=o_f\tK\x0b6C"
+    bool_0 = False
+    module_0.reverse_string(bool_0)
 
 
 def test_case_4():
-    none_type_0 = None
-    module_0.fahrenheit_to_celsius(none_type_0)
+    var_0 = module_0.random_stats()
+    module_0.fahrenheit_to_celsius(var_0)
 
 
 def test_case_5():
     calculator_0 = module_0.Calculator()
     assert calculator_0.memory == 0
-    calculator_0.multiply(calculator_0, calculator_0)
+    var_0 = calculator_0.clear()
+    assert var_0 == 0
+    module_0.fahrenheit_to_celsius(calculator_0)
 
 
 def test_case_6():
     calculator_0 = module_0.Calculator()
     assert calculator_0.memory == 0
-    var_0 = calculator_0.clear()
-    assert var_0 == 0
-    var_1 = calculator_0.add(var_0, var_0)
-    assert var_1 == 0
+    calculator_0.multiply(calculator_0, calculator_0)
 
 
 def test_case_7():
-    none_type_0 = None
-    var_0 = module_0.average(none_type_0)
-    assert var_0 == 0
+    float_0 = -3081.12628
+    with pytest.raises(ValueError):
+        module_0.factorial(float_0)
 
 
 def test_case_8():
-    bool_0 = False
-    var_0 = module_0.factorial(bool_0)
-    assert var_0 == 1
-    module_0.random_stats(bool_0)
+    calculator_0 = module_0.Calculator()
+    assert calculator_0.memory == 0
+    calculator_0.add(calculator_0, calculator_0)
 
 
 def test_case_9():
-    int_0 = -1181
-    with pytest.raises(ValueError):
-        module_0.factorial(int_0)
-
-
-@pytest.mark.xfail(strict=True)
-def test_case_10():
     calculator_0 = module_0.Calculator()
     assert calculator_0.memory == 0
-    int_0 = 2232
-    module_0.factorial(int_0)
+    none_type_0 = None
+    bool_0 = False
+    var_0 = module_0.average(bool_0)
+    assert var_0 == 0
+    calculator_0.add(var_0, none_type_0)
+
+
+def test_case_10():
+    float_0 = 951.0
+    var_0 = module_0.factorial(float_0)
+    assert var_0 == pytest.approx(1e309, abs=0.01, rel=0.01)
